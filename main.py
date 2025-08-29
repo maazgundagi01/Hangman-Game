34fb2eadc6cbebb5becf99a3926344db07b8328e
Maz Gunn <maazgundagi@gmail.com>
2025-08-29 00:37:20 -0500

    Added function to check the current status of guessed word and print blanks in place of unguessed word, Started developing the core game loop

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import words
 import diagrams
 
+#Picks a random word from the array of words in the "word" module
 def pick_a_string():
     random_int = random.randint(0, len(words.guess_words) - 1)
     word = words.guess_words[random_int]
@@ -9,7 +10,38 @@ def pick_a_string():
 
 random_word = pick_a_string()
 
+# For comparison, we need the string split into set of unique characters
+set_of_characters = set(random_word)
 
+#Come back to this function when reading the while loop.
+#This block distinguishes guessed and unguessed letters in the word user is guessing and outputs the word as combination of blanks and letters
+def print_blanks(current_answer):
+    blanks = []
+    for letter in set_of_characters:
+        if letter in current_answer:
+            blanks.append(letter)
+        else:
+            blanks.append("_")
+    word_status = "".join(blanks)
+    return word_status
 
+#These variables track score
+user_lives = 7
+answer = {}
+wrong_answers = {}
+print(diagrams.hangman_text)
+print(f"\nWelcome to Hangman!\nHangman is a simple game, you have to guess all the letters of the word\nyou will be eliminated from the game if you guess more than 6 wrong words!\nLet's Begin!\n")
 
+while answer != set_of_characters:
+    print(f'{diagrams.decoration_line} Lives:{user_lives - 1} {diagrams.decoration_line}')
 
+    print(f'Letters not in the word: {",".join(wrong_answers)}')
+    character = input("Type a letter that you think exists in the word! --> ")
+    if character in set_of_characters:
+        if character not in answer:
+            answer.add(character)
+            print('Yes! The letter is in the word!')
+        else:
+            print('Nice try but you already guessed that character! No lives were deducted, try a different character!')
+    else:
+        wrong_answers.add(character)
