ad7e1af017b5a422d417fbd32b361fac9ecf6209
Maz Gunn <maazgundagi@gmail.com>
2025-08-29 00:39:00 -0500

    added decoration outline drawing

--- diagrams.py
+++ diagrams.py
@@ -56,3 +56,18 @@ diagram = [
       |
 ========='''
 ]
+hangman_text = [
+'''
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+_                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                       
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+'''
+]
+decoration_line = ['XXXXXXXXXXXXXXXXXXXXXXXX']
